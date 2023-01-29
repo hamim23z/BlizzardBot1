4b7c7a0f5b5d7641caf854e4e4340e5de0dc71db
Hamim <74880822+hamim23z@users.noreply.github.com>
2023-01-28 19:57:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ module. There are a number of utility commands being showcased here.'''
 
 intents = discord.Intents.default()
 intents.members = True
-intents.message_content = True
 
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
@@ -18,8 +17,11 @@ process1 = subprocess.Popen(["python", "channel.py"]) # Create and launch proces
 process2 = subprocess.Popen(["python", "shoesize.py"])
 process3 = subprocess.Popen(["python", "website.py"])
 process4 = subprocess.Popen(["python", "companies.py"])
+process5 = subprocess.Popen(["python", "ebayviews.py"])
+
 
 process1.wait() # Wait for process1 to finish (basically wait for script to finish)
 process2.wait()
 process3.wait()
 process4.wait()
+process5.wait()
